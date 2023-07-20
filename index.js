975b7a2bc1cb22c8007f3f2eec0c0989b9f2961b
Sergio Heredia <sergioheredia1996@gmail.com>
2023-07-19 23:47:07 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+let main = document.querySelector("main");
+main.remove();
+let newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "Mike Tyson is the champion";
+
+
